3604ecdd0a3a45bb459496a8aeb05216e793e8e4
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2026-03-23 13:41:29 +0100

    chore: remove unused files (Privacy, Error, ValidationScripts)

--- SoftwareRouteur/Program.cs
+++ SoftwareRouteur/Program.cs
@@ -34,7 +34,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Index");
     app.UseHsts();
 }
 
