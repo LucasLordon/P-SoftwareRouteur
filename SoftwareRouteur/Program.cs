77fff22d61d71c8c4bfe81e2b5747b2158866203
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2026-04-22 16:33:46 +0200

    feat: add OPNsense configuration section to appsettings

--- SoftwareRouteur/Program.cs
+++ SoftwareRouteur/Program.cs
@@ -2,13 +2,21 @@ using Microsoft.EntityFrameworkCore;
 using SoftwareRouteur.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using SoftwareRouteur.Models;
+using SoftwareRouteur.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var password = Environment.GetEnvironmentVariable("DB_PASSWORD")
                ?? builder.Configuration["DB_PASSWORD"];
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+var apiKey = Environment.GetEnvironmentVariable("API_KEY")
+              ?? builder.Configuration["API_KEY"];
+
+var apiSecret = Environment.GetEnvironmentVariable("API_SECRET")
+               ?? builder.Configuration["API_SECRET"];
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!
     .Replace("${DB_PASSWORD}", password);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -21,6 +29,11 @@ builder.Services.AddLocalization(options => options.ResourcesPath = "Resources")
 builder.Services.AddControllersWithViews(options =>
     options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()))
     .AddViewLocalization();
+
+builder.Services.Configure<OPNsenseSettings>(
+    builder.Configuration.GetSection("OPNsense"));
+
+builder.Services.AddSingleton<OPNsenseService>();
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
@@ -39,7 +52,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 var supportedCultures = new[] { "fr", "en", "de" };
 app.UseRequestLocalization(new RequestLocalizationOptions()
@@ -51,11 +64,10 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
